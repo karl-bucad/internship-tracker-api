7f913ef7da9cde6050d0ce480e95cae4763f80b2
Karl Bucad <bucadkarl@gmail.com>
2026-06-30 09:54:32 -0400

    Add about and applications endpoints

--- main.py
+++ main.py
@@ -7,3 +7,14 @@ def root():
     return { 
         "message" : "Internship Tracker API"
     }
+
+@app.get("/about")
+def about():
+    return {
+        "creator" : "Karl Bucad",
+        "project" : "Internship Tracker API"
+    }
+
+@app.get("/applications")
+def applications():
+    return []
