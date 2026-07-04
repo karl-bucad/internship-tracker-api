608eade53b7cad1c5ed87293f2ef45860d321bb1
Karl Bucad <bucadkarl@gmail.com>
2026-07-03 21:33:13 -0400

    Add JWT authentication and protect endpoints

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI, HTTPException
 from database import get_connection
 from models import Application, User, LoginUser
 from passlib.context import CryptContext
+from jose import jwt, JWTError
+from datetime import datetime, timedelta, timezone
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
 
 app = FastAPI()
 
@@ -10,11 +14,46 @@ pwd_context = CryptContext(
     deprecated = "auto"
 )
 
+SECRET_KEY = "temporary-secret-key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl = "/login")
+
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.now(timezone.utc) + timedelta(minutes = ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm = ALGORITHM)
+
+    return encoded_jwt
+
+def get_current_user_id(token: str = Depends(oauth2_scheme)) -> int:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms = [ALGORITHM])
+        user_id = payload.get("user_id")
+
+        if user_id is None:
+            raise HTTPException(
+                status_code = 401,
+                detail = "Invalid token"
+            )
+        
+        return user_id
+    
+    except JWTError:
+        raise HTTPException(
+            status_code = 401,
+            detail = "Invalid token"
+        )
 
 @app.get("/")
 def root():
@@ -30,7 +69,7 @@ def about():
     }
 
 @app.get("/applications")
-def get_applications():
+def get_applications(current_user_id: int = Depends(get_current_user_id)):
     connection = get_connection()
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM applications")
@@ -199,11 +238,11 @@ def login_user(user: LoginUser):
             detail = "Invalid email or password"
         )
 
+    access_token = create_access_token(
+        data = {"user_id": row["id"]}
+    )
+
     return {
-        "message": "Login successful",
-        "user": {
-            "id": row["id"],
-            "username": row["username"],
-            "email": row["email"]
-        }
+        "access_token": access_token,
+        "token_type": "bearer"
     }
