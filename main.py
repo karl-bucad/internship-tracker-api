7f30a4a5d1a5edc135df2caa9db324441d1e5b83
Karl Bucad <bucadkarl@gmail.com>
2026-07-04 14:54:08 -0400

    Enable CORS for frontend

--- main.py
+++ main.py
@@ -10,10 +10,18 @@ from security import (
 )
 from routers.applications import router as applications_router
 from routers.auth import router as auth_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(applications_router)
 app.include_router(auth_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
